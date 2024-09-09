4ea5c7d8e794c1741d5bda7fd262feaed02b1aaa
Kishor1703 <pskishor196@gmail.com>
2024-09-09 12:12:37 +0530

    all delete

--- frontend/src/components/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { isAuthenticated } from '../helpers/auth';
-
-const PrivateRoute = ({ component: Component }) => {
-  return isAuthenticated() ? <Component /> : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
