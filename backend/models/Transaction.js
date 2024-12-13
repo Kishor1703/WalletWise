0b71925a0b73623e3e1a2a7e04c6e59b61118bd5
Kishor1703 <pskishor196@gmail.com>
2024-12-13 13:36:25 +0530

    Debugged

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -12,7 +12,7 @@ const transactionSchema = new mongoose.Schema({
     },
     category: {
         type: String,
-        required: false,
+        required: true,
     },
     type: {
         type: String,
