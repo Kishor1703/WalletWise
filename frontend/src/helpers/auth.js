c1a9ac91e016eaae5a41d8a9b62fbc23d9ec950c
Kishor1703 <pskishor196@gmail.com>
2024-12-19 12:46:21 +0530

    Basic

--- frontend/src/helpers/auth.js
+++ frontend/src/helpers/auth.js
@@ -1,5 +1,5 @@
-export const isAuthenticated = () => {
-    const token = localStorage.getItem('token');
-    return !!token;
-  };
+// export const isAuthenticated = () => {
+//     const token = localStorage.getItem('token');
+//     return !!token;
+//   };
   
